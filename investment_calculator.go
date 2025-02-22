7dc8bab516c0102c298ca488d3aff0f50a4b5cca
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-22 13:24:59 +0300

    Functions exercise

--- investment_calculator.go
+++ investment_calculator.go
@@ -1,52 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
-
-const inflationRate = 2.5
+import "fmt"
 
 func main() {
-	//Example
-	//var a, b, c, d int = 10, 5.6, "test", true
-	//fmt.Println(a, b, c, d)
+	revenue := getUserInput("Revenue: ")
+	expenses := getUserInput("Expenses: ")
+	taxRate := getUserInput("Tax Rate: ")
 
-	var investmentAmount float64 = 1000
+	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
-	//Example
-	//var expectedReturnRate = 5.5
-
-	expectedReturnRate := 5.5
-	var years float64 = 10
-
-	fmt.Print("Investment Amount: ")
-	fmt.Scan(&investmentAmount)
-	outputText("Expected Return Rate: ")
-	fmt.Scan(&expectedReturnRate)
-	outputText("Years: ")
-	fmt.Scan(&years)
-	//Example
-	//var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
-
-	//var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	//futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
-
-	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
-
-	formattedFV := fmt.Sprintf("Future Value: %.1f\n", futureValue)
-	formattedFRV := fmt.Sprintf("Future Value (adjasted for Inflation): %.1f\n", futureRealValue)
-	fmt.Print(formattedFV, formattedFRV)
-
-	//fmt.Printf("Future Value: %.1f\nFuture Value (adjasted for Inflation): %.1f\n", futureValue, futureRealValue)
+	fmt.Printf("%.1f\n", ebt)
+	fmt.Printf("%.1f\n", profit)
+	fmt.Printf("%.3f\n", ratio)
 }
 
-func outputText(text string) {
+func getUserInput(text string) (value float64) {
 	fmt.Print(text)
+	fmt.Scan(&value)
+	return
 }
-
-func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64) {
-	fv := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
-	frv := fv / math.Pow(1+inflationRate/100, years)
-	return fv, frv
+func calculateFinancials(revenue, expenses, taxRate float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return
 }
